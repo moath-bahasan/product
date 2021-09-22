9226467dab8820c710f494dd5b797f2eeec17d89
moath bahasan <moazbahasan@gmail.com>
2021-09-22 20:31:54 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,14 @@
+// شيفرة الشرط الخاصة بتشغيل الفيديو
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+    if (myVideo.paused) 
+        myVideo.play();
+    else 
+        myVideo.pause(); 
+}
+$(document).ready(function() {
+    $(function () {
+        $("#commentForm").validate();
+    });
+});
